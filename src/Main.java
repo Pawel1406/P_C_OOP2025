f55c55318e3ac534babde7a740715278c908d0a6
P_C_OOP2025 <platyn14@gmail.com>
2025-02-25 19:01:09 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
         Point p1 = new Point();
         p1.x = 10;
         p1.y = 20;
-        System.out.println("twój x to " + p1.x + " a twój y to "+ p1.y);
+       // System.out.println(p1.toSvg());// p1.toString()
+        p1.translate(400,500);
+        System.out.println(p1);
+        Point p2=p1.translated(237,0);
+        System.out.println(p2);
     }
 }
