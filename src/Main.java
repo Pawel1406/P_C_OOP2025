d56376c9f021821ea26b91b4f2adaac0f3b14c4a
P_C_OOP2025 <platyn14@gmail.com>
2025-02-25 17:52:44 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome i ajja");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Point p1 = new Point();
+        p1.x = 10;
+        p1.y = 20;
+        System.out.println("twój x to " + p1.x + " a twój y to "+ p1.y);
     }
 }
